018cfe04e1f33296ef4898ee3107a4847866e2f0
SahsiSunny <sahsisunny@gmail.com>
2022-11-18 10:56:00 +0530

    initial commit
    add all files

---
+++ script.js
@@ -0,0 +1,83 @@
+'use strict';
+
+const checkBTN = document.getElementById('btn');
+const resetBTN = document.getElementById('reset');
+
+const inputDate = document.getElementById('date');
+const luckyNumber = document.getElementById('lucky');
+
+const inputArea = document.querySelector('.input-area');
+const resultArea = document.querySelector('.result-area');
+
+const resultText = document.getElementById('result-text');
+
+const aniForLucky = document.querySelector('.container');
+
+
+// Function for showing result
+function showResult() {
+     resultArea.classList.remove("hide");
+     inputArea.classList.add("hide");
+}
+
+let date;
+let lucky;
+let sum = 0;
+
+// function to set result and show result
+function setAndShowResult(msg) {
+     resultText.innerText = msg;
+     showResult();
+}
+
+const getDate = () => {
+     date = inputDate.value;
+     if (date === "") {
+          setAndShowResult("Please enter your birthdate");
+     }
+     return date;
+}
+const getLuckynumber = () => {
+     lucky = luckyNumber.value;
+     if (lucky === "" || lucky <= 0) {
+          setAndShowResult("Please enter a valid lucky number");
+     }
+     return lucky;
+}
+
+const calculateSum = () => {
+     let date = getDate();
+     for (let i = 0; i < date.length; i++) {
+          if (date[i] !== "-") {
+               sum = sum + Number(date[i]);
+          }
+     }
+     return sum;
+}
+
+
+function isLucky() {
+     const lucky = getLuckynumber();
+     const sum = calculateSum();
+
+     if (sum % lucky === 0) {
+          setAndShowResult("Your birthday is lucky");
+          aniForLucky.classList.add("animation");
+     }
+     else {
+          setAndShowResult("Your birthday is not lucky");
+          aniForLucky.classList.add("not-lucky");
+     }
+}
+
+function clickHandler() {
+     if (inputDate.value === "" || luckyNumber.value === "") {
+          setAndShowResult("Please enter both the fields");
+     }
+     else {
+          isLucky();
+     }
+}
+
+checkBTN.addEventListener('click', clickHandler);
+resetBTN.addEventListener('click', () => window.location.reload());
